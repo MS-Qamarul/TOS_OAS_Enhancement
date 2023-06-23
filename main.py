dfb3608331bbe8f734b270500fe5637103305f26
Qamarul-Fattah <qamarul@mindstretcher.com>
2023-06-23 16:04:01 +0800

    Optimal values

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import numpy as np
 import os
 
 # Value to darken image
-alpha_value = 155.0 # 155.0 is optimal value so far <<< CHANGE THIS IF NECESSARY
+alpha_value = 175.0 # 175.0 is optimal value so far <<< CHANGE THIS IF NECESSARY
+value_2 = 0.0 # 0.0 is optimal value so far <<< CHANGE THIS IF NECESSARY
 
 def enhance_contrast(image):
     # Convert the image to grayscale
@@ -13,18 +14,18 @@ def enhance_contrast(image):
     min_intensity = np.min(gray)
     max_intensity = np.max(gray)
     stretched = cv2.convertScaleAbs(gray, alpha = alpha_value / (max_intensity - min_intensity),
-                                    beta=-min_intensity * alpha_value / (max_intensity - min_intensity))
+                                    beta=-min_intensity * value_2 / (max_intensity - min_intensity))
 
     return stretched
 
-def convert_tif_to_png(input_path, output_path):
+def process_to_tif(input_path, output_path):
     # Read the TIF image
     image = cv2.imread(input_path)
 
     # Enhance contrast
     enhanced_image = enhance_contrast(image)
 
-    # Save the enhanced image as PNG
+    # Save the enhanced image as TIF
     cv2.imwrite(output_path, enhanced_image)
 
 # Folders
@@ -36,19 +37,19 @@ if not os.path.exists(input_folder):
     os.mkdir(input_folder)
     print("Folder %s created!" % input_folder)
 else:
-    print("Folder %s already exists" % input_folder)
+    print("Folder %s already exist" % input_folder)
 
 # Check whether directory already exists
 if not os.path.exists(output_folder):
     os.mkdir(output_folder)
     print("Folder %s created!" % output_folder)
 else:
-    print("Folder %s already exists" % output_folder)
+    print("Folder %s already exist" % output_folder)
 
 # Iterate over TIF files in the input folder
 for filename in os.listdir(input_folder):
     if filename.endswith(".tif"):
         input_path = os.path.join(input_folder, filename)
-        output_path = os.path.join(output_folder, filename[:-4] + ".png")  # Change file extension to PNG
-        print(filename[:-4] + ".png created!")
-        convert_tif_to_png(input_path, output_path)
+        output_path = os.path.join(output_folder, filename[:-4] + ".tif")  # Change file extension to PNG
+        print(filename[:-4] + ".tif created!")
+        process_to_tif(input_path, output_path)
