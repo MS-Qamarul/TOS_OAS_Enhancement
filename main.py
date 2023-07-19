3e5430477d229a246a897bfbbd3a1a5682e9fba6
Qamarul-Fattah <qamarul@mindstretcher.com>
2023-07-19 11:53:27 +0800

    Subfolder support
    Added support to read from subfolders, although there are issues when it contains Chinese characters. No work around for now.

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import numpy as np
 import os
 
 # Adjustable values to darken or lighten image for enhance_contrast
-alpha_value = 175.0 # 175.0 is optimal value so far <<< CHANGE THIS IF NECESSARY
-value_2 = 0.0 # 0.0 is optimal value so far <<< CHANGE THIS IF NECESSARY
+alpha_value = 155.0  # 155.0 is optimal value so far <<< CHANGE THIS IF NECESSARY
+value_2 = 0.0  # 0.0 is optimal value so far <<< CHANGE THIS IF NECESSARY
 
 # Define enhance_contrast
 def enhance_contrast(image):
@@ -14,12 +14,15 @@ def enhance_contrast(image):
     # Apply contrast stretching
     min_intensity = np.min(gray)
     max_intensity = np.max(gray)
-    stretched = cv2.convertScaleAbs(gray, alpha = alpha_value / (max_intensity - min_intensity),
-                                    beta=-min_intensity * value_2 / (max_intensity - min_intensity))
+    stretched = cv2.convertScaleAbs(
+        gray,
+        alpha=alpha_value / (max_intensity - min_intensity),
+        beta=-min_intensity * value_2 / (max_intensity - min_intensity),
+    )
 
     return stretched
 
-# Define process_to_tiff (or whatever image format you want to convert to)
+# Define process_to_tif (or whatever image format you want to convert to)
 def process_to_tif(input_path, output_path):
     # Read the TIF image
     image = cv2.imread(input_path)
@@ -36,26 +39,33 @@ output_folder = "02_Output"
 
 print("=============== START ===============")
 
-# Check whether directory already exists
+# Check whether directories already exist
 if not os.path.exists(input_folder):
     os.mkdir(input_folder)
     print("Folder %s created!" % input_folder)
 else:
-    print("Folder %s already exist" % input_folder)
+    print("Folder %s already exists" % input_folder)
 
-# Check whether directory already exists
 if not os.path.exists(output_folder):
     os.mkdir(output_folder)
     print("Folder %s created!" % output_folder)
 else:
-    print("Folder %s already exist" % output_folder)
+    print("Folder %s already exists" % output_folder)
 
-# Iterate over TIF files in the input folder
-for filename in os.listdir(input_folder):
-        if filename.endswith(".tif"):
-            input_path = os.path.join(input_folder, filename)
-            output_path = os.path.join(output_folder, filename[:-4] + ".tif") # Set file extension
-            print(filename[:-4] + ".tif created!")
-            process_to_tif(input_path, output_path)
+# Iterate over subfolders in the input folder
+for subfolder_name in os.listdir(input_folder):
+    subfolder_path = os.path.join(input_folder, subfolder_name)
+    if os.path.isdir(subfolder_path):
+        output_subfolder_path = os.path.join(output_folder, subfolder_name)
+        os.makedirs(output_subfolder_path, exist_ok=True)
+        print(f"Subfolder '{subfolder_name}' created!")
+
+        # Iterate over TIF files in the subfolder
+        for filename in os.listdir(subfolder_path):
+            if filename.endswith(".tif"):
+                input_path = os.path.join(subfolder_path, filename)
+                output_path = os.path.join(output_subfolder_path, filename)
+                print(f"Processing '{filename}'")
+                process_to_tif(input_path, output_path)
 
 print("=============== END ===============")
